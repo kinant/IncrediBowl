978b6fb0114e78cac0b2d593fa28782bf428bca3
Kinan Turman <kinan.t@gmail.com>
2017-05-08 21:45:17 -0600

    Pin Fixes
    Fixed some bugs with the pins.

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+
+    private Rigidbody m_rb;
+
+	// Use this for initialization
+	void Awake () {
+
+	}
+
+    private void Start()
+    {
+        m_rb = GetComponent<Rigidbody>();
+        m_rb.isKinematic = false;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        m_rb.velocity = Vector3.zero;
+        m_rb.angularVelocity = Vector3.zero;
+    }
+}
