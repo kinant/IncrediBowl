a2d7eb871a8dc05dbbc87193953d07dcd91060ab
Kinan Turman <kinan.t@gmail.com>
2017-05-22 12:17:01 -0600

    Speech Recognition
    Added speech recognition. It will allow the user to change the ball color to red, green or blue and also allow the user to spawn a cola, chocolate or pizza.

---
+++ Assets/Scripts/SpeechRecognizer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class SpeechRecognizer : MonoBehaviour {
+
+    public string[] keywords = new string[] { "cola", "pizza", "chocolate", "red", "green", "blue" };
+    public ConfidenceLevel confidence;
+
+    public GameObject pizzaPrefab;
+    public GameObject colaPrefab;
+    public GameObject chocoPrefab;
+
+    public GameObject ball;
+
+    private PhraseRecognizer recognizer;
+    private Material ballMaterial;
+
+    protected string phraseSaid = "right";
+
+	// Use this for initialization
+	void Start () {
+        if (keywords != null) {
+            recognizer = new KeywordRecognizer(keywords, confidence);
+            recognizer.OnPhraseRecognized += HandlePhraseRecognized;
+            recognizer.Start();
+        }
+
+        if (ball != null) {
+            ballMaterial = ball.GetComponent<Renderer>().material;
+        }
+
+	}
+
+    private void OnApplicationQuit()
+    {
+        if (recognizer != null && recognizer.IsRunning) {
+            recognizer.OnPhraseRecognized -= HandlePhraseRecognized;
+            recognizer.Stop();
+        }
+    }
+
+    private void HandlePhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        phraseSaid = args.text;
+        Debug.Log("WILL SPAWN: " + phraseSaid);
+
+        switch (phraseSaid) {
+            case "red":
+                ballMaterial.color = Color.red;
+                break;
+            case "green":
+                ballMaterial.color = Color.green;
+                break;
+            case "blue":
+                ballMaterial.color = Color.blue;
+                break;
+            default:
+                break;
+        }
+
+    }
+}
