37dc66dab790299f6e5ab9dc7a8f8e2120bea082
Kinan Turman <kinan.t@gmail.com>
2017-05-22 12:38:25 -0600

    Movie Player
    Added Easy Movie Texture + Videos to show when scoring a strike or a spare

---
+++ Assets/LargeScreenMoviePlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LargeScreenMoviePlayer : MonoBehaviour {
+
+    private MediaPlayerCtrl mediaPlayerCtrl;
+
+	// Use this for initialization
+	void Start () {
+
+        mediaPlayerCtrl = GetComponent<MediaPlayerCtrl>();
+
+        if (mediaPlayerCtrl != null) {
+            mediaPlayerCtrl.Play();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
